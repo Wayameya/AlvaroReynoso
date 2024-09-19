b7ff482c445705295d34f96e27327b95b1b8cb81
Wayameya <0254125@up.edu.mx>
2024-09-18 21:17:54 -0600

    Pt2 (not finished

--- Simon/Simon/script.js
+++ Simon/Simon/script.js
@@ -21,16 +21,13 @@ function nextSequence() {
 }
 
 function playSound(color) {
-    // Each color has a corresponding sound file
     var audio = new Audio("sounds/" + color + ".mp3");
     audio.play();
 }
 
 function animatePress(color) {
-    // Add the pressed class to the button
     $("#" + color).addClass("pressed");
 
-    // Remove the pressed class after 100ms
     setTimeout(function() {
         $("#" + color).removeClass("pressed");
     }, 100);
@@ -43,7 +40,7 @@ $(".btn").click(function() {
     animatePress(userChosenColor);
     console.log("User clicked: " + userChosenColor);
     console.log("User's sequence: " + userClickedPattern);;
-    
+
     setTimeout(function() {
         checkAnswer(userClickedPattern.length - 1);
     }, 100);
@@ -55,36 +52,46 @@ $(document).keydown(function() {
 });
 
 function checkAnswer(currentLevel) {
+    // Log the user's pattern and the game pattern for debugging
+    console.log("Checking answer for level: " + currentLevel);
+    console.log("User's pattern: " + userClickedPattern);
+    console.log("Game pattern: " + gamePattern);
+
     // Check if the most recent user's click matches the corresponding game pattern
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-        // Correct answer
+        console.log("Correct so far!");
+
+        // Check if the user has finished the whole sequence
         if (userClickedPattern.length === gamePattern.length) {
-            // User has completed the sequence, proceed to the next sequence
+            console.log("Sequence complete!");
             setTimeout(function() {
                 userClickedPattern = [];
                 nextSequence();
             }, 1000);
         }
     } else {
-        // Incorrect answer
+        console.log("Wrong answer!");
+
         playSound("wrong");
 
-        // Add a red flash to the <body> to indicate game over
         $("body").addClass("game-over");
 
         setTimeout(function() {
             $("body").removeClass("game-over");
         }, 200);
 
-        // Reset the game
         $("#level-title").text("Game Over, Press Any Key to Restart");
 
-        // Restart the game
         startOver();
     }
 }
 
-// Function to restart the game
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+}
+
 function startOver() {
     level = 0;
     gamePattern = [];
